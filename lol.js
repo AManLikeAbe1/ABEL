5ca1b847d32f093fbdb8da482282ccb8addef240
AManLikeAbe1 <160073090+AManLikeAbe1@users.noreply.github.com>
2024-02-14 21:06:42 +0000

    Add files via upload

---
+++ lol.js
@@ -0,0 +1,54 @@
+var quote = new Array();
+quote[0]="The laws of gravity cannot be held responsible for people falling in love. ~Albert Einstein";
+quote[1]="When you are courting a nice girl an hour seems like a second. When you sit on a red-hot cinder a second seems like an hour. That's relativity. ~Albert Einstein";
+quote[2]="Love is composed of a single soul inhabiting two bodies. ~Aristotle";
+quote[3]="Let us always meet each other with smile, for the smile is the beginning of love. ~Mother Teresa";
+quote[4]="All you need is love. ~John Lennon";
+quote[5]="The greatest thing you'll ever learn Is to love and be loved in return. ~Natalie Cole";
+quote[6]="He that falls in love with himself will have no rivals. ~Benjamin Franklin";
+quote[7]="For it was not into my ear you whispered, but into my heart. It was not my lips you kissed, but my soul. ~Judy Garland";
+quote[8]="Love is an irresistible desire to be irresistibly desired. ~ Robert Frost";
+quote[9]="It's a long road when you face the world alone, when no one reaches out a hand for you to hold. You can find love if you search within your soul, and the emptiness you felt will disappear. ~Mariah Carey";
+quote[10]="You're all I need, my love, my Valentine. ~Martina McBride";
+quote[11]="I feel wonderful because I see the love light in your eyes. ~Eric Clapton";
+quote[12]="Do all things with love. ~ Og Mandino";
+quote[13]="I was born with an enormous need for affection, and a terrible need to give it. ~Audrey Hepburn";
+quote[14]="A part of kindness consists in loving people more than they deserve. ~Joseph Joubert";
+quote[15]="Love is a force more formidable than any other. It is invisible - it cannot be seen or measured, yet it is powerful enough to transform you in a moment, and offer you more joy than any material possession could. ~Barbara de Angelis";
+quote[16]="A loving heart is the truest wisdom. ~ Charles Dickens";
+quote[17]="Love is life. And if you miss love, you miss life. ~Leo Buscaglia";
+quote[18]="I like not only to be loved, but also to be told I am loved. ~George Eliot";
+quote[19]="Love is always bestowed as a gift - freely, willingly and without expectation. We don't love to be loved; we love to love. ~Leo Buscaglia";
+quote[20]="Love yourself first and everything else falls into line. You really have to love yourself to get anything done in this world. ~Lucille Ball";
+quote[21]="'Tis better to have loved and lost than never to have loved at all. ~Alfred Lord Tennyson";
+quote[22]="Love is the beauty of the soul. ~ Saint Augustine";
+quote[23]="The one thing we can never get enough of is love. And the one thing we never give enough is love. ~Henry Miller";
+quote[24]="Love isn't something you find. Love is something that finds you. ~Loretta Young";
+quote[25]="Love is a game that two can play and both win. ~Eva Gabor";
+quote[26]="The moment you have in your heart this extraordinary thing called love and feel the depth, the delight, the ecstasy of it, you will discover that for you the world is transformed. ~Jiddu Krishnamurti";
+quote[27]="You will find as you look back upon your life that the moments when you have truly lived are the moments when you have done things in the spirit of love. ~Henry Drummond";
+quote[28]="For small creatures such as we the vastness is bearable only through love. ~Carl Sagan";
+quote[29]="Looking back, I have this to regret, that too often when I loved, I did not say so. ~David Grayson";
+quote[30]="What we have once enjoyed we can never lose. All that we love deeply becomes a part of us. ~Helen Keller";
+quote[31]="Fortune and love favor the brave. ~ Ovid";
+quote[32]="Blessed is the influence of one true, loving human soul on another. ~George Eliot";
+quote[33]="To love oneself is the beginning of a lifelong romance. ~Oscar Wilde";
+quote[34]="There is no remedy for love but to love more. ~Henry David Thoreau";
+quote[35]="A very small degree of hope is sufficient to cause the birth of love. ~Stendhal";
+quote[36]="Love cures people – both the ones who give it and the ones who receive it. ~Dr. Karl Menninger";
+quote[37]="Once you truly believe you're worthy of love, you will never settle for anyone's second best treatment. ~Charles J. Orlando";
+quote[38]="If a thing loves, it is infinite. ~William Blake";
+quote[39]="The best and most beautiful things in this world cannot be seen or even heard, but must be felt with the heart. ~Helen Keller";
+quote[40]="We are most alive when we're in love. ~John Updike";
+quote[41]="And in the end, the love you take, is equal to the love you make. ~Paul McCartney";
+
+
+
+
+function randomLoveQuote(){
+randomQuote=quote[Math.floor(Math.random()*quote.length)]
+	document.form.input.value=randomQuote
+}
+
+
+randomLoveQuote()
